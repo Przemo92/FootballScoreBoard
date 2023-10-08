dd0ca7b0eaaa2387814f90a9f967c573314dc931
Przemek Kapela <przemyslaw.kapela@appverk.com>
2023-10-08 19:18:06 +0200

    first red test

---
+++ tests/FootballScoreBoardTest.php
@@ -0,0 +1,11 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+class FootballScoreBoardTest extends TestCase
+{
+    public function testStartGame()
+    {
+        $scoreBoard = new FootballScoreBoard();
+    }
+}
